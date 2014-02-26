a0375a4cb79aacac49c1153ae2fe8a190ab829bb
mindrenee <rwitsenburg@MacBook-Pro-van-R.local>
2014-02-26 22:34:09 +0100

    Add App

---
+++ Hangman/Hangman/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hangman
+//
+//  Created by R Witsenburg on 26-02-14.
+//  Copyright (c) 2014 R Witsenburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
